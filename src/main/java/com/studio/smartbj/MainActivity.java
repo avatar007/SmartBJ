06bb703bef7eeb2e69a6737e880166fb4bf482a9
木水 <avatar_007@qq.com>
2017-03-13 23:25:33 +0800

    第一次修改

--- src/main/java/com/studio/smartbj/MainActivity.java
+++ src/main/java/com/studio/smartbj/MainActivity.java
@@ -1,13 +1,19 @@
 package com.studio.smartbj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("我是智慧北京的第一个页面");
+        tv.setTextColor(Color.RED);
+        tv.setTextSize(40);
+        setContentView(tv);
     }
 }
